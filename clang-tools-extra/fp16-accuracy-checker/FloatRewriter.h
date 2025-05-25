8db89d3b4ad6646223dc63d0e081345b742ea9e9
Goutami Sooda <soodagoutami@gmail.com>
2025-05-25 18:37:35 +0530

    Refactor the code to support different float usage

--- clang-tools-extra/fp16-accuracy-checker/FloatRewriter.h
+++ clang-tools-extra/fp16-accuracy-checker/FloatRewriter.h
@@ -2,25 +2,62 @@
 #define FLOAT_REWRITER_H
 
 #include "clang/AST/RecursiveASTVisitor.h"
-#include "clang/AST/ASTConsumer.h"  
+#include "clang/AST/ASTConsumer.h"
 #include "clang/Rewrite/Core/Rewriter.h"
-#include "llvm/Support/raw_ostream.h"
-#include "clang/Frontend/CompilerInstance.h"
 
-class FloatRewriter : public clang::ASTConsumer,
-                      public clang::RecursiveASTVisitor<FloatRewriter> {
+using namespace clang;
+
+class FloatRewriter : public ASTConsumer, public RecursiveASTVisitor<FloatRewriter> {
 public:
-    FloatRewriter(clang::Rewriter &R, const std::string &TargetFloatType);
+    FloatRewriter(Rewriter &R, std::string TargetType)
+        : TheRewriter(R), TargetFloatType(std::move(TargetType)) {}
 
-    bool VisitVarDecl(clang::VarDecl *Declaration);
-    bool VisitFunctionDecl(clang::FunctionDecl *Declaration);
+    void HandleTranslationUnit(ASTContext &Context) override {
+        this->Context = &Context;
+        TraverseDecl(Context.getTranslationUnitDecl());
+    }
 
-    void HandleTranslationUnit(clang::ASTContext &Context) override;
+    bool VisitVarDecl(VarDecl *VD) {
+        QualType QT = VD->getType();
+        if (QT.getAsString() == "float") {
+            SourceLocation SL = VD->getTypeSpecStartLoc();
+            TheRewriter.ReplaceText(SL, 5, TargetFloatType);
+        }
+        return true;
+    }
+
+    bool VisitFieldDecl(FieldDecl *FD) {
+        QualType QT = FD->getType();
+        if (QT.getAsString() == "float") {
+            SourceLocation SL = FD->getTypeSpecStartLoc();
+            TheRewriter.ReplaceText(SL, 5, TargetFloatType);
+        }
+        return true;
+    }
+
+    bool VisitParmVarDecl(ParmVarDecl *PVD) {
+        QualType QT = PVD->getType();
+        if (QT.getAsString() == "float") {
+            SourceLocation SL = PVD->getTypeSpecStartLoc();
+            TheRewriter.ReplaceText(SL, 5, TargetFloatType);
+        }
+        return true;
+    }
+
+    bool VisitFunctionDecl(FunctionDecl *FD) {
+        QualType RT = FD->getReturnType();
+        if (RT.getAsString() == "float") {
+            SourceLocation SL = FD->getReturnTypeSourceRange().getBegin();
+            TheRewriter.ReplaceText(SL, 5, TargetFloatType);
+        }
+        return true;
+    }
 
 private:
-    clang::Rewriter &TheRewriter;
-    std::string TargetType;
+    Rewriter &TheRewriter;
+    ASTContext *Context;
+    std::string TargetFloatType;
 };
 
-#endif
+#endif // FLOAT_REWRITER_H
 
