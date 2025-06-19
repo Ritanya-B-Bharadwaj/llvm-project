ee1bbbc9370839cddc0a744ae159c1163cff5b42
BSSJAYANTH <165804857+BSSJAYANTH@users.noreply.github.com>
2025-06-19 17:02:21 +0530

    Update ClangAnnotator.cpp

--- llvm/tools/clang-annotator/ClangAnnotator.cpp
+++ llvm/tools/clang-annotator/ClangAnnotator.cpp
@@ -1,21 +1,23 @@
 #include "clang/AST/ASTConsumer.h"
 #include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Basic/SourceManager.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendActions.h"
 #include "clang/Tooling/Tooling.h"
-#include "clang/Basic/SourceManager.h"
 #include "llvm/Support/raw_ostream.h"
 
-#include <map>
-#include <vector>
 #include <fstream>
+#include <map>
 #include <sstream>
+#include <vector>
 
 using namespace clang;
 
 class AnnotatorVisitor : public RecursiveASTVisitor<AnnotatorVisitor> {
 public:
-  explicit AnnotatorVisitor(ASTContext *Context, std::map<unsigned, std::vector<std::string>> &LineAnnotations)
+  explicit AnnotatorVisitor(
+      ASTContext *Context,
+      std::map<unsigned, std::vector<std::string>> &LineAnnotations)
       : Context(Context), LineAnnotations(LineAnnotations) {}
 
   bool VisitStmt(Stmt *S) {
@@ -32,8 +34,7 @@ private:
   ASTContext *Context;
   std::map<unsigned, std::vector<std::string>> &LineAnnotations;
 
-  template <typename T>
-  void annotate(T *Node, const std::string &KindName) {
+  template <typename T> void annotate(T *Node, const std::string &KindName) {
     SourceLocation Loc = Node->getBeginLoc();
     if (Context->getSourceManager().isWrittenInMainFile(Loc)) {
       unsigned line = Context->getSourceManager().getSpellingLineNumber(Loc);
@@ -44,7 +45,9 @@ private:
 
 class AnnotatorConsumer : public ASTConsumer {
 public:
-  explicit AnnotatorConsumer(ASTContext *Context, std::map<unsigned, std::vector<std::string>> &LineAnnotations)
+  explicit AnnotatorConsumer(
+      ASTContext *Context,
+      std::map<unsigned, std::vector<std::string>> &LineAnnotations)
       : Visitor(Context, LineAnnotations) {}
 
   void HandleTranslationUnit(ASTContext &Context) override {
@@ -64,46 +67,46 @@ public:
     return true;
   }
 
-  std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI, StringRef InFile) override {
-    return std::make_unique<AnnotatorConsumer>(&CI.getASTContext(), LineAnnotations);
+  std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI,
+                                                 StringRef InFile) override {
+    return std::make_unique<AnnotatorConsumer>(&CI.getASTContext(),
+                                               LineAnnotations);
   }
 
   void EndSourceFileAction() override {
-  auto FileEntryRef = SourceMgr->getFileEntryRefForID(SourceMgr->getMainFileID());
-  if (!FileEntryRef) {
-    llvm::errs() << "Could not get file name.\n";
-    return;
-  }
-  std::string filename = std::string(FileEntryRef->getName());
-
-  std::ifstream input(filename);
-  std::string line;
-  unsigned lineno = 1;
-
-  while (std::getline(input, line)) {
-    llvm::outs() << lineno << ": " << line;
-    auto it = LineAnnotations.find(lineno);
-    if (it != LineAnnotations.end()) {
-      llvm::outs() << "  // AST Nodes: [";
-      for (size_t i = 0; i < it->second.size(); ++i) {
-        llvm::outs() << "'" << it->second[i] << "'";
-        if (i + 1 < it->second.size())
-          llvm::outs() << ", ";
-      }
-      llvm::outs() << "]";
+    auto FileEntryRef =
+        SourceMgr->getFileEntryRefForID(SourceMgr->getMainFileID());
+    if (!FileEntryRef) {
+      llvm::errs() << "Could not get file name.\n";
+      return;
     }
-    llvm::outs() << "\n";
-    ++lineno;
+    std::string filename = std::string(FileEntryRef->getName());
+
+    std::ifstream input(filename);
+    std::string line;
+    unsigned lineno = 1;
+
+    while (std::getline(input, line)) {
+      llvm::outs() << lineno << ": " << line;
+      auto it = LineAnnotations.find(lineno);
+      if (it != LineAnnotations.end()) {
+        llvm::outs() << "  // AST Nodes: [";
+        for (size_t i = 0; i < it->second.size(); ++i) {
+          llvm::outs() << "'" << it->second[i] << "'";
+          if (i + 1 < it->second.size())
+            llvm::outs() << ", ";
+        }
+        llvm::outs() << "]";
+      }
+      llvm::outs() << "\n";
+      ++lineno;
+    }
   }
-}
-
-
 
 private:
   std::map<unsigned, std::vector<std::string>> LineAnnotations;
   clang::SourceManager *SourceMgr;
 };
-
 
 int main(int argc, const char **argv) {
   if (argc > 1) {
@@ -117,7 +120,8 @@ int main(int argc, const char **argv) {
     std::string code = buffer.str();
 
     std::vector<std::string> args = {"clang-annotator", "-std=c++17"};
-    clang::tooling::runToolOnCodeWithArgs(std::make_unique<AnnotatorAction>(), code, args, argv[1]);
+    clang::tooling::runToolOnCodeWithArgs(std::make_unique<AnnotatorAction>(),
+                                          code, args, argv[1]);
   } else {
     llvm::errs() << "Usage: clang-annotator <source_file>\n";
   }
