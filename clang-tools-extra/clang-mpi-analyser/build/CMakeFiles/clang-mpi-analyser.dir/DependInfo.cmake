63d23ccb3bc704262d2c5217a8d1b2d39a0f5819
Vishwanath Bhat <vishwasbhat2003@gmail.com>
2025-06-20 14:06:38 +0000

    Added Clang MPI Analyser tool

---
+++ clang-tools-extra/clang-mpi-analyser/build/CMakeFiles/clang-mpi-analyser.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/d/OMG/LLVM/llvm-project/clang-tools-extra/clang-mpi-analyser/tool/MPIAction.cpp" "CMakeFiles/clang-mpi-analyser.dir/tool/MPIAction.cpp.o" "gcc" "CMakeFiles/clang-mpi-analyser.dir/tool/MPIAction.cpp.o.d"
+  "/mnt/d/OMG/LLVM/llvm-project/clang-tools-extra/clang-mpi-analyser/tool/MPIAnalysisHelperFuncs.cpp" "CMakeFiles/clang-mpi-analyser.dir/tool/MPIAnalysisHelperFuncs.cpp.o" "gcc" "CMakeFiles/clang-mpi-analyser.dir/tool/MPIAnalysisHelperFuncs.cpp.o.d"
+  "/mnt/d/OMG/LLVM/llvm-project/clang-tools-extra/clang-mpi-analyser/tool/MPIAnalyzer.cpp" "CMakeFiles/clang-mpi-analyser.dir/tool/MPIAnalyzer.cpp.o" "gcc" "CMakeFiles/clang-mpi-analyser.dir/tool/MPIAnalyzer.cpp.o.d"
+  "/mnt/d/OMG/LLVM/llvm-project/clang-tools-extra/clang-mpi-analyser/tool/main.cpp" "CMakeFiles/clang-mpi-analyser.dir/tool/main.cpp.o" "gcc" "CMakeFiles/clang-mpi-analyser.dir/tool/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
